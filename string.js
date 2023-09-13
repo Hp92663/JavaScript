1e7048dfd76c089a1bed6b5979658858ceeb89e0
Hp92663 <88971001+Hp92663@users.noreply.github.com>
2023-09-13 20:12:46 +0000

    add string file

---
+++ string.js
@@ -0,0 +1,57 @@
+//  string -   collection of characterics
+
+//  In string that can 2 way to create a string 
+// 1. Single quotes  ->  ''
+// 2. Double quotes  ->  ""
+
+let sname = 'Khut'
+let name = "Hiren";
+
+console.log(sname); // print 'Khut'
+console.log(name); // print 'Hiren'
+console.log(name.length); // print the variable length - here '5'
+console.log(name[0]); // print the first character of 'Hiren' - 'H'
+console.log(name[1]); // print the first character of 'Hiren' - 'i'
+
+console.log("\n");
+
+
+// Template literals
+// it can use backtick  -> ``  insted of quotes to define string
+
+// here can be use multiple 'quotes' in between 'backtick'
+// for exmaple
+
+let ex =  ` this is "a" backtick example`;
+
+
+//  here directly print variable using '${}' method
+// it's also called "string interpolation"
+
+let s = "CPPGICA"
+let example =  `${name} is a student of ${s}`
+
+console.log(example); // print 'Hiren'
+
+
+console.log("\n");
+
+
+// Escape Sequence  -> \, \n, \t, \r
+
+let a = "Hello \" World"  // '\'  - for describe symbol & quote
+console.log(a); // print Hello " world
+
+console.log("\n");
+
+
+let b = "Hello \n World"  // '\n'  - for new line
+console.log(b); // print Hello " world
+
+
+
+let c = "Hello \t World"  // '\t'  - for tab
+console.log(c); 
+
+let d = "Hello \r World"  // '\r'  - carriage return
+console.log(d); 
