2b09e0d39f64b47ac8074a8ac869501bed509b62
Hiren Khut <88971001+Hp92663@users.noreply.github.com>
2023-10-21 03:33:19 +0530

    Update promises.js

--- promises.js
+++ promises.js
@@ -78,7 +78,7 @@ x
 
 //  .then() & catch()
 
-
+/*
 let x = new Promise((resolve, reject) => {
   console.log("Promises pending ")
   setTimeout(() => {
@@ -95,3 +95,65 @@ x
 .catch((error) =>{
   console.log(error)
 })
+
+*/
+
+
+
+
+
+function getChesse(){
+  return new Promise((resolve, reject) =>{
+   setTimeout(() =>{
+      const chesse = "🧀"
+      // console.log("here is chesse", chesse);
+      resolve(chesse);
+   }, 2000);
+  })
+}
+
+function makeBase(chesse){
+  return new Promise((resolve, reject) =>{
+    setTimeout(() =>{
+      const base = chesse + "🍪"
+      // console.log("here is base", base);
+      resolve(base); 
+      // reject("cheese is not good")
+    },2000);
+  })
+}
+
+function readyPizza(base){
+  return new Promise((resolve, reject) =>{
+    setTimeout(()=>{
+      const pizza = base + "🍕";
+      // console.log("Here is pizza", pizza);
+      // resolve(pizza)
+       reject("pizza is not ready");
+      
+    }, 2000);
+  })
+}
+
+getChesse()
+.then((chesse) =>{
+  console.log("here is chesse", chesse);
+  return makeBase(chesse);
+})
+.then((base) =>{
+  console.log("here is base", base);
+  return readyPizza(base);
+})
+.then((pizza) =>{
+  console.log("Here is pizza", pizza);
+})
+
+.catch((error) =>{
+  console.log("Some Error occupied", error);
+})
+
+.finally(() =>{
+    console.log("Process is End");
+})
+
+
